d3318d810a4d2c4a34beb05552c09bfc48cd1e4f
jjoaquimsx <mf.joaquim2005@gmail.com>
2023-10-19 18:44:00 -0300

    condicoes em js

---
+++ MODULO-04/aula1/script.js
@@ -0,0 +1,8 @@
+var vel = 66
+console.log(`A velocidade do seu carro e ${vel}Km/h`)
+
+if(vel > 60){//condicao simples!
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO!!')
+}
+
+console.log('Dirija com cuidado!!')
---
+++ MODULO-04/aula1/script02.js
@@ -0,0 +1,9 @@
+var pais = 'Irã'
+
+console.log(`Se voce mora no ${pais} voce é`)
+
+if(pais == 'Brasil'){
+    console.log('Nativo, Uhul!!')
+} else{
+    console.log('Estrangeiro')
+}
