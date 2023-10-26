d5b16efc9f92688e2464629be03be92763146f61
jjoaquimsx <mf.joaquim2005@gmail.com>
2023-10-26 18:41:05 -0300

    horas

--- MODULO-04/exercicio/script.js
+++ MODULO-04/exercicio/script.js
@@ -4,7 +4,6 @@ function carregar(){
     var mens = document.getElementById('mensage')
     var data = new Date()
     var hora = data.getHours()
-
     var min = new Date()
     var minutos = min.getMinutes()
 
@@ -12,22 +11,21 @@ function carregar(){
 
     if(hora >= 0 && hora < 12){
         //bom dia
-        img.src = 'assets/Manha.png'
+        img.src = 'Manha.png'
         mens.innerText = 'Bom dia'
-        document.body.style.background = 'background-image: linear-gradient(to top, #fddb92 0%, #d1fdff 100%);'
-        document.body.style.transition = "1s linear"
-
+        document.getElementById('container').style.background = 'linear-gradient(to right, #00416a, #e4e5e6)'
+        document.getElementById('container').style.transition = "all 2s linear"
     } else if(hora >= 12 && hora <= 18){
         //tarde
         img.src = 'tarde.png'
         mens.innerText = 'Boa tarde'
-        document.body.style.background = 'background-image: linear-gradient( 109.6deg,  rgba(245,56,56,1) 11.2%, rgba(234,192,117,1) 78% );'
-        document.body.style.transition = "1s linear"
+        document.getElementById('container').style.background = 'linear-gradient( 109.6deg,  rgba(245,56,56,1) 11.2%, rgba(234,192,117,1) 78% )'
+        document.getElementById('container').style.transition = 'all 2s linear'
     }else{
         //boa noite
         img.src = 'noite.png'
         mens.innerText = 'Boa noite'
-        document.body.style.background = 'background: linear-gradient(140deg, rgba(66, 73, 141, 1) 48%, rgba(0, 212, 255, 1) 100%);'
-        document.body.style.transition = "1s linear"
+        document.getElementById('container').style.background = 'linear-gradient(140deg, rgba(66, 73, 141, 1) 48%, rgba(0, 212, 255, 1) 100%)'
+        document.getElementById('container').style.transition = " all 2s linear"
     }
 }
