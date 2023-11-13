37f41d2d5bba0a4548e618e1f22dbbc61e044544
jjoaquimsx <mf.joaquim2005@gmail.com>
2023-11-13 17:25:03 -0300

    inicio do desafio

---
+++ MODULO-05/exercicios/script.js
@@ -0,0 +1,7 @@
+function contar(){
+    var ini = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0)
+}
