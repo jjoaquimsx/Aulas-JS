93025796208f56929d99a9f247e9be83ad622569
jjoaquimsx <mf.joaquim2005@gmail.com>
2023-12-15 08:04:10 -0300

    uso de arrays

---
+++ MODULO-05/ambiente03.js
@@ -0,0 +1,20 @@
+let num = [3,4,1,5]
+
+num.sort()
+
+console.log(`O valor do meu vetor é ${num}`)
+
+num[4] = 5 /*adiciona um novo valor a uma posicao*/
+
+console.log(num)
+
+console.log(`O vetor tem ${num.length} indices, foi adicionado 1 indice 2 linhas de codigo acima`)
+
+num.sort()
+
+console.log(`para coloca-los em order crecente ${num.sort()} colocamos o codigo da linha acima`)
+
+num[4]
+
+console.log(`Para verificar apenas um numero dos vetores ${num[3]} utilizamos o codigo acima`)
+
